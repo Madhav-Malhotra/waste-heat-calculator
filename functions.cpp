145d37c95d0308dd64abf10783be8a6e817841fa
Madhav Malhotra <47190635+Madhav-Malhotra@users.noreply.github.com>
2022-09-29 11:42:54 -0400

    Math functions defined

--- functions.cpp
+++ functions.cpp
@@ -17,18 +17,90 @@
 int main();
 float water_mass_weight(float weight);
 float water_mass_volume(float volume);
-float* temp_to_array(float temp_datapoint);
+float* temp_to_array(float temp_datapoint, float* temp_array);
 float energy_transferred(float* temp_array, float water_mass);
 float power(float energy, float time);
 float efficiency(float useful_power, float rated_power);
 float annual_energy(float power, float daily_use, int yearly_use);
 float waste_energy(float energy, float efficiency);
-float energy_running_cost(float energy, float unit_price);
-float ROI(float initial_cost, float savings_per_year);
+float energy_variable_cost(float energy, float unit_price);
+float ROI(float initial_cost, float annual_savings);
 
 
 
 // FUNCTION DEFINITIONS
 int main() {
   return 0;
+}
+
+float water_mass_weight(float weight) {
+  return 0;
+}
+
+float water_mass_volume(float volume) {
+  return 0;
+}
+
+float* temp_to_array(float temp_datapoint, float* temp_array) {
+  return 0;
+}
+
+float energy_transferred(float* temp_array, float water_mass) {
+  return 0;
+}
+
+
+// @brief                - Calculates energy consumption over time
+// @param energy         - joules of energy consumed
+// @param time           - seconds of time passed
+// @return power         - watts of power 
+float power(float energy, float time) {
+  return energy / time;
+}
+
+
+// @brief                - Calculates ratio of useful heat to waste heat
+// @param useful_power   - watts of power transferred to boiling water
+// @param rated_power    - total watts of power consumed
+// @return efficiency    - unitless ratio of useful power to total power
+float efficiency(float useful_power, float rated_power) {
+  return useful_power / rated_power;
+}
+
+
+// @brief                - Calculates energy consumed per year
+// @param power          - watts of power consumed per second
+// @param daily_use      - hours stove is used per day
+// @param yearly_use     - days stove is used per year
+// @return               - joules of energy consumed per year
+float annual_energy(float power, float daily_use, int yearly_use) {
+  int time_in_seconds = 3600 * daily_use * yearly_use;
+  return power * time_in_seconds;
+}
+
+
+// @brief                - Calculates wasted portion of energy consumption 
+// @param energy         - joules of total energy consumption
+// @param efficiency     - unitless ratio of waste energy to total energy consumption
+// @return               - joules of energy wasted per year
+float waste_energy(float energy, float efficiency) {
+  return energy * (1 - efficiency);
+}
+
+
+// @brief                - Calculates variable cost from energy consumption 
+// @param energy         - joules of total energy consumption
+// @param unit_price     - price of energy in $CAD per kilowatt-hour
+// @return               - cost of energy consumption in $CAD
+float energy_variable_cost(float energy, float unit_price) {
+  return energy * unit_price;
+}
+
+
+// @brief                - Calculates years of use before breaking even on initial investment 
+// @param initial_cost   - upfront price in $CAD for purchasing stove 
+// @param annual_savings - cost of waste energy consumption avoided per year
+// @return               - Number of years until annual savings = initial cost
+float ROI(float initial_cost, float annual_savings) {
+  return initial_cost / annual_savings;
 }
